d60105fbea853c54f34296162f4e66df0d833741
Hardik Patel <hardikgw@gmail.com>
2017-07-04 07:23:31 -0400

    toree

---
+++ spark-yarn/firststart.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+export SPARK_HOME=/home/hadoop/spark/spark
+export YARN_CONF_DIR=/home/hadoop/hadoop/etc/hadoop
+export PYSPARK_DRIVER_PYTHON=ipython
+export PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser --port=7777"
+
+hdfs namenode -format
+./home/hadoop/hadoop/start.sh
+
+jupyter notebook --port=8888 --no-browser --ip=0.0.0.0 --allow-root
