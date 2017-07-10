ab4ecb1669d176c68e1f293661284010f1321b40
Hardik Patel <hardikgw@gmail.com>
2017-07-10 09:16:15 -0700

    memory

--- spark-yarn/firststart.sh
+++ spark-yarn/firststart.sh
@@ -4,6 +4,7 @@ export SPARK_HOME=/home/hadoop/spark/spark
 export YARN_CONF_DIR=/home/hadoop/hadoop/etc/hadoop
 export PYSPARK_DRIVER_PYTHON=ipython
 export PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser --port=7777"
+export SPARK_EXECUTOR_MEMORY=3g
 
 hdfs namenode -format
 ./home/hadoop/hadoop/start.sh
