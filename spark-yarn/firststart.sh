93ab2425e717c175b23698516832c5b99f5df740
Hardik Patel <hardikgw@gmail.com>
2017-07-13 12:22:59 -0400

    ipython3

--- spark-yarn/firststart.sh
+++ spark-yarn/firststart.sh
@@ -2,9 +2,9 @@
 
 export SPARK_HOME=/home/hadoop/spark/spark
 export YARN_CONF_DIR=/home/hadoop/hadoop/etc/hadoop
-export PYSPARK_DRIVER_PYTHON=ipython
-export PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser --port=7777"
-export SPARK_EXECUTOR_MEMORY=3g
+export PYSPARK_DRIVER_PYTHON=ipython3
+export PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser --port=8888"
+export SPARK_EXECUTOR_MEMORY=1g
 
 hdfs namenode -format
 ./home/hadoop/hadoop/start.sh
