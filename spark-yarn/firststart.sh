ad2562c50d88a52b655d903b2d6a9f8aaf1ac6bd
Hardik Patel <hardikgw@gmail.com>
2017-07-25 21:21:21 -0400

    cluster

--- spark-yarn/firststart.sh
+++ spark-yarn/firststart.sh
@@ -8,7 +8,7 @@ export PYSPARK_SUBMIT_ARGS="--packages graphframes:graphframes:0.5.0-spark2.1-s_
 export PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser --port=8888"
 export SPARK_EXECUTOR_MEMORY=1g
 
-hdfs namenode -format
+hdfs namenode -format -nonInteractive
 ./home/hadoop/hadoop/start.sh
 
 jupyter notebook --port=8888 --no-browser --ip=0.0.0.0 --allow-root
