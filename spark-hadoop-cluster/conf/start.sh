7b5bd0b8c5d6e2a6ec94c9da1b71614fb86d8f1e
Hardik Patel <hardikgw@gmail.com>
2017-07-25 22:52:20 -0400

    shell fix

--- spark-hadoop-cluster/conf/start.sh
+++ spark-hadoop-cluster/conf/start.sh
@@ -7,9 +7,15 @@ export PYSPARK_DRIVER_PYTHON=ipython3
 export PYSPARK_SUBMIT_ARGS="--packages graphframes:graphframes:0.5.0-spark2.1-s_2.11,com.databricks:spark-csv_2.11:1.2.0"
 export PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser --port=8888"
 export SPARK_EXECUTOR_MEMORY=1g
+
+ssh-keygen -R localhost
+ssh-keygen -R master
+ssh-keygen -R slave1
+ssh-keygen -R slave2
+
 hdfs namenode -format -nonInteractive
 ./home/hadoop/hadoop/start.sh
 
-if [ $1 == "master" ]; then
+if [ $1 = "master" ]; then
 	jupyter notebook --port=8888 --no-browser --ip=0.0.0.0 --allow-root
 fi
