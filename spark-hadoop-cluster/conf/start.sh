40e8009d613b51af65874e02071a57e3ad3422d8
Hardik Patel <hardikgw@gmail.com>
2017-07-26 00:36:08 -0400

    rsa

--- spark-hadoop-cluster/conf/start.sh
+++ spark-hadoop-cluster/conf/start.sh
@@ -8,15 +8,19 @@ export PYSPARK_SUBMIT_ARGS="--packages graphframes:graphframes:0.5.0-spark2.1-s_
 export PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser --port=8888"
 export SPARK_EXECUTOR_MEMORY=1g
 
-ssh-keygen -R localhost
-ssh-keygen -R 0.0.0.0
-ssh-keygen -R master
-ssh-keygen -R slave1
-ssh-keygen -R slave2
-
 hdfs namenode -format -nonInteractive
+
+if [ $1 = "master" ]; then
+	ssh-keygen -t rsa
+	cp .ssh/id_rsa.pub /.ssh/authorized_keys
+fi
+
 ./home/hadoop/hadoop/start.sh
 
 if [ $1 = "master" ]; then
 	jupyter notebook --port=8888 --no-browser --ip=0.0.0.0 --allow-root
+fi
+
+if [ $1 = "slave" ]; then
+	echo "slave"
 fi
