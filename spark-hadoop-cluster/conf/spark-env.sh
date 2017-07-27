02fb6ae1de6179c32bf961fe865cbe08cde4b724
Hardik Patel <hardikgw@gmail.com>
2017-07-26 23:10:28 -0400

    spark slaves and startup

---
+++ spark-hadoop-cluster/conf/spark-env.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export PYSPARK_PYTHON=python
+export PYTHONPATH=:/home/hadoop/spark/spark/graphframes.jar:.
+export SPARK_MASTER_HOST=master
--- spark-hadoop-cluster/conf/start.sh
+++ spark-hadoop-cluster/conf/start.sh
@@ -7,12 +7,14 @@ export PYSPARK_DRIVER_PYTHON=ipython3
 export PYSPARK_SUBMIT_ARGS="--packages graphframes:graphframes:0.5.0-spark2.1-s_2.11,com.databricks:spark-csv_2.11:1.2.0"
 export PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser --port=8888"
 export SPARK_EXECUTOR_MEMORY=1g
+export SPARK_MASTER_HOST=master
 
 service ssh start
 
 if [ $1 = "master" ]; then
 	hdfs namenode -format -nonInteractive
 	./home/hadoop/hadoop/start.sh
+	./home/hadoop/spark/spark/sbin/start-all.sh
 	jupyter notebook --port=8888 --no-browser --ip=0.0.0.0 --allow-root
 fi
 
