c2b2de7dd4de36e537bac48d401c4a8d3fd07a02
Hardik Patel <hardikgw@gmail.com>
2018-12-04 13:40:22 -0500

    init

--- solr/solr-entrypoint.sh
+++ solr/solr-entrypoint.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-
-set -e
-
-if [[ ! -z "$(SOLR_INIT)" ]]; then
-    ./opt/solr/server/scripts/cloud-scripts/zkcli.sh -zkhost $(ZK_HOST) -cmd makepath /solr
-    ./opt/solr/server/bin/solr zk upconfig -n cit_default -d $(SOLR_CONF)/_default -z $(ZK_HOST)
-    ./opt/solr/server/bin/solr zk cp file:$(SOLR_CONF)/solr.xml zk:/solr/solr.xml -z $(ZK_HOST)
+if [[ ! -z "$SOLR_INIT" ]]; then
+    echo "Setting up ZK"
+    ./opt/solr/server/scripts/cloud-scripts/zkcli.sh -zkhost $ZK_HOST -cmd makepath /solr
+    ./opt/solr/server/bin/solr zk upconfig -n cit_default -d $SOLR_CONF/_default -z $(ZK_HOST)
+    ./opt/solr/server/bin/solr zk cp file:${SOLR_CONF}/solr.xml zk:/solr/solr.xml -z $(ZK_HOST)
 fi
-
-exec "$@"
