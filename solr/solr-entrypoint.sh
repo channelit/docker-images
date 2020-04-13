b14642eca958bab7f5509ee75e4c5d47c4324da3
Hardik <hardikgw@gmail.com>
2020-04-13 17:56:02 -0400

    solr

--- solr/solr-entrypoint.sh
+++ solr/solr-entrypoint.sh
@@ -2,6 +2,7 @@
 if [[ ! -z "$SOLR_INIT" ]]; then
     echo "Setting up ZK"
     echo "ZK_HOST is " $ZK_HOST
+    echo "SOLR_CONF is " $SOLR_CONF
     ./server/scripts/cloud-scripts/zkcli.sh -zkhost $ZK_HOST -cmd makepath /solr
     ./bin/solr zk upconfig -n cit_default -d "$SOLR_CONF"/_default -z "$ZK_HOST"/solr -r
     ./bin/solr zk cp file:"$SOLR_CONF"/solr.xml zk:/solr/solr.xml -z "$ZK_HOST"
